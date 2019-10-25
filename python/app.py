d9c0eb1c2adc77a49137459495e8b040134ba33e
Hope Tambala <hopetambala@outlook.com>
2019-10-25 14:26:00 -0400

    chore: add comment

--- python/app.py
+++ python/app.py
@@ -1,6 +1,11 @@
 import plotly.express as px
 import plotly.express as px
 
+
+#X year
+#Y Tons
+
+#each line is a year
 gapminder = px.data.gapminder().query("continent=='Europe'")
 fig = px.line_3d(gapminder, x="gdpPercap", y="pop", z="year", color='country')
 fig.show()
